d67a3c3ba4069eba8ba997a85c44dfdf3e9af4dc
yellowb <122517167@qq.com>
2017-07-12 14:49:39 +0800

    Add Sample for prototype inheritance

---
+++ src/js-object/inheritance.js
@@ -0,0 +1,31 @@
+/**
+ * Created by HUANGYE2 on 7/12/2017.
+ */
+
+/**
+ * Sample for prototype inheritance
+ * @param age
+ * @constructor
+ */
+
+function Animal(age) {
+    this.age = age;
+}
+Animal.prototype.getAge = function() {
+    console.log('Age: %s', this.age);
+};
+
+function Human(age, name) {
+    Animal.call(this, age);
+    this.name = name;
+}
+Human.prototype = Object.create(Animal.prototype);  // Link Animal as Human's parent
+Human.prototype.getAgeAndName = function () {
+    this.getAge();  // call parent's function
+    console.log('Name: %s', this.name);
+};
+
+var tom = new Human(20, 'Tom');
+tom.getAgeAndName();
+tom.getAge();
+
