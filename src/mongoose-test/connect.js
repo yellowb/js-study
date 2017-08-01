0308ede677658e8c36c4f360a4f425c573bd3add
yellowb <122517167@qq.com>
2017-08-01 11:07:17 +0800

    update mongoose quick-start sample

--- src/mongoose-test/connect.js
+++ src/mongoose-test/connect.js
@@ -5,31 +5,41 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-
-var promise = mongoose.connect('mongodb://localhost:27017/test', {
-    useMongoClient: true
-    /* other options */
-});
-promise.then(function(db) {
+// Connect to mongodb
+mongoose.connect(
+    'mongodb://localhost:27017/test',
+    {useMongoClient: true}
+).then(function (db) {
     console.log('DB connected!');
-    console.log(db);
-}).catch(function(err) {
+}).catch(function (err) {
     console.log('DB connect failed!');
     console.log(err.stack);
 });
 
+// Build schema, model, document
 var kittySchema = mongoose.Schema({
-    name:   { type: String, lowercase: true },
-    age:     { type: Number, min: 18, max: 65 },
+    name: {type: String},
+    age: {type: Number},
 });
-var Kitten = mongoose.model('Kitten', kittySchema);
-var silence = new Kitten({ name: 'Chick', age: 20 });
-silence.save(function (err, silence) {
-    if (err) return console.error(err);
-    else console.log(JSON.stringify(silence, null, 2));
-});
-Kitten.find({}).exec().then(function(docs) {
-    console.log(docs[0].toJSON());
-    docs[4].age = 29;
-    docs[4].save();
-});
+var KittyModel = mongoose.model('Kitten', kittySchema);
+var kitty = new KittyModel(
+    {name: 'Kitty', age: 20}
+);
+
+// Save the new document, then find out all documents after saving done.
+kitty.save()
+    .then(function (newKitty) {
+        console.log('_id of new kitty: ' + newKitty._id);
+        return KittyModel.find({}).exec();
+    })
+    .then(function (kitties) {
+        console.log('Now we have kitties:');
+        console.log(JSON.stringify(kitties, null, 2));
+    })
+    .then(function () {
+        // Disconnect mongodb.
+        return mongoose.disconnect();
+    })
+    .catch(function (err) {
+        console.error(err.stack);
+    });
